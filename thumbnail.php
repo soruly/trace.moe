f5e508a27d4a9d1a4c1dd8136c73bf712dcc9958
soruly <soruly@gmail.com>
2016-07-02 15:15:33 +0800

    Remove extracted thumbnail after search

--- thumbnail.php
+++ thumbnail.php
@@ -9,4 +9,5 @@ $filepath = '/mnt/Data/Anime New/'.$season.'/'.$anime.'/'.$file;
 exec("ffmpeg -y -ss ".$t." -i \"$filepath\" -vframes 1 -vf scale=300:-1 -f image2 thumbnail/".$uuid.".jpg");
 header('Content-type: image/jpeg');
 readfile($thumbdir.$uuid.'.jpg');
+unlink($thumbdir.$uuid.'.jpg');
 ?>
