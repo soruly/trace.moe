5e8c307890f1a4da9aeecb5683a706ffacdbfef6
soruly <soruly@gmail.com>
2017-04-22 20:54:46 +0800

    Fix undefined index on search API param

--- api/search.php
+++ api/search.php
@@ -132,7 +132,7 @@ if(isset($_POST['image'])){
             $filter = str_replace('"','',rawurldecode($_POST['filter']));
         }
         $max_trial = 6;
-        if(intval($_POST['trial'])){
+        if(isset($_POST['trial']) && intval($_POST['trial'])){
             $max_trial = intval($_POST['trial']) > 12 ? 12 : intval($_POST['trial']);
         }
         $trial = 3;
