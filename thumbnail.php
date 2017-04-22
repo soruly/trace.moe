43a01f46d9e8fa6b1e1766f58a7f6512c219c53f
soruly <soruly@gmail.com>
2017-04-22 20:52:30 +0800

    Add missing thumbnail image for missing video

--- thumbnail.php
+++ thumbnail.php
@@ -1,6 +1,8 @@
 <?php
 require 'vendor/autoload.php';
 
+header('Content-type: image/jpeg');
+
 $thumbdir = 'thumbnail/';
 $uuid = uniqid();
 $t = floatval($_GET['t']);
@@ -9,24 +11,32 @@ $anime = rawurldecode($_GET['anime']);
 $file = rawurldecode($_GET['file']);
 $filepath = '/mnt/Data/Anime New/'.$season.'/'.$anime.'/'.$file;
 $thumbpath = $thumbdir.$uuid.'.jpg';
-
-$ffmpeg = FFMpeg\FFMpeg::create();
-$video = $ffmpeg->open($filepath);
-$video
-  ->frame(FFMpeg\Coordinate\TimeCode::fromSeconds($t))
-  ->save($thumbpath);
-
-header('Content-type: image/jpeg');
-
-list($width, $height) = getimagesize($thumbpath);
 $new_width = 320;
 $new_height = 180;
 
-$image_p = imagecreatetruecolor($new_width, $new_height);
-$image = imagecreatefromjpeg($thumbpath);
-imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+try {
+  $ffmpeg = FFMpeg\FFMpeg::create();
+  $video = $ffmpeg->open($filepath);
+  $video
+    ->frame(FFMpeg\Coordinate\TimeCode::fromSeconds($t))
+    ->save($thumbpath);
 
-imagejpeg($image_p, null, 80);
+  list($width, $height) = getimagesize($thumbpath);
 
-unlink($thumbpath);
+  $image_p = imagecreatetruecolor($new_width, $new_height);
+  $image = imagecreatefromjpeg($thumbpath);
+  imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+
+  imagejpeg($image_p, null, 80);
+
+  unlink($thumbpath);
+  imagedestroy($image_p);
+} catch (Exception $e) {
+
+  $image_p = imagecreatetruecolor($new_width, $new_height);
+  $text_color = imagecolorallocate($image_p, 255, 255, 255);
+  imagestring($image_p, 3, 5, 5,  'Preview unavailable', $text_color);
+  imagejpeg($image_p, null, 80);
+  imagedestroy($image_p);
+}
 ?>
