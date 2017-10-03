72d3a4ff0e56e7f62ddcf76465be954e03041e51
soruly <soruly@gmail.com>
2017-10-03 17:46:12 +0800

    Add 2017-10

--- index.php
+++ index.php
@@ -174,6 +174,7 @@ Browse a file <input type="file" id="file" name="files[]" />
 <option value="2017-01/*">2017-01</option>
 <option value="2017-04/*">2017-04</option>
 <option value="2017-07/*">2017-07</option>
+<option value="2017-10/*">2017-10</option>
 </select>
 
 <button id="safeBtn" type="button" class="btn btn-default btn-sm">
