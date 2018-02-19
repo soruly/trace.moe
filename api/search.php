84dd08f867b61c356518d95e5c54ef1115ad89b8
soruly <soruly@gmail.com>
2018-02-19 22:41:23 +0800

    Fix titles for API

--- api/search.php
+++ api/search.php
@@ -313,7 +313,7 @@ if(isset($_POST['image'])){
                     // use anilist ID to get titles of different languages
                     $request = array(
                     "size" => 1,
-                    "_source" => array("title"),
+                    "_source" => array("title", "synonyms", "synonyms_chinese"),
                     "query" => array(
                         "ids" => array(
                             "values" => array(intval($anilist_id))
@@ -330,13 +330,13 @@ if(isset($_POST['image'])){
                         $res = curl_exec($curl);
                         $result = json_decode($res);
                         if($result->hits && $result->hits->total > 0){
-                            $doc->title = $result->hits->hits[0]->_source->title->native ? $result->hits->hits[0]->_source->title->native : null;
-                            $doc->title_native = $result->hits->hits[0]->_source->title->native ? $result->hits->hits[0]->_source->title->native : null;
-                            $doc->title_chinese = $result->hits->hits[0]->_source->title->chinese ? $result->hits->hits[0]->_source->title->chinese : null;
-                            $doc->title_english = $result->hits->hits[0]->_source->title->english ? $result->hits->hits[0]->_source->title->english : null;
-                            $doc->title_romaji = $result->hits->hits[0]->_source->title->romaji ? $result->hits->hits[0]->_source->title->romaji : null;
-                            $doc->synonyms = $result->hits->hits[0]->_source->synonyms ? $result->hits->hits[0]->_source->synonyms : [];
-                            $doc->synonyms_chinese = $result->hits->hits[0]->_source->synonyms_chinese ? $result->hits->hits[0]->_source->synonyms_chinese : [];
+                            $doc->title_romaji = $result->hits->hits[0]->_source->title->romaji ?? $anime;
+                            $doc->title_native = $result->hits->hits[0]->_source->title->native ?? $doc->title_romaji;
+                            $doc->title_english = $result->hits->hits[0]->_source->title->english ?? $doc->title_romaji;
+                            $doc->title_chinese = $result->hits->hits[0]->_source->title->chinese ?? $anime;
+                            $doc->title = $doc->title_native;
+                            $doc->synonyms = $result->hits->hits[0]->_source->synonyms;
+                            $doc->synonyms_chinese = $result->hits->hits[0]->_source->synonyms_chinese;
                         }
                     }
                     catch(Exception $e){
