f2d8b0de3c237fe8ae59787aef4519292001a499
soruly <soruly@gmail.com>
2018-03-28 14:02:20 +0800

    Update terms

--- terms.php
+++ terms.php
@@ -64,6 +64,7 @@ It is our policy, in appropriate circumstances and at its discretion, to disable
 </p>
 <p>&nbsp;</p>
 <a href="//www.iubenda.com/privacy-policy/7764846" class="iubenda-white iubenda-embed" title="Privacy Policy">Privacy Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src = "//cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+<p>All images uploaded to search engine will be deleted within 24 hours.</p>
 <p>&nbsp;</p>
 </div>
 
