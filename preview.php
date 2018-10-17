99bfb71d3e6ea4fd17e299ac770e3f9edb344927
soruly <soruly@gmail.com>
2018-10-17 17:41:47 +0800

    Add mute param to generate mute video

--- preview.php
+++ preview.php
@@ -14,9 +14,10 @@ if ($season && $anime) {  // deprecated
   $filepath = str_replace('`', '\`', '/mnt/data/anime_new/'.$season.'/'.$anime.'/'.$file);
 }
 $thumbpath = $thumbdir.$uuid.'.mp4';
+$mute = isset($_GET['mute']) ? "-an" : "";
 
 if(file_exists($filepath)){
-    exec("ffmpeg -y -ss ".$start." -i \"$filepath\" -to ".$duration." -vf scale=640:-1 -c:v libx264 -preset fast ".$thumbpath);
+    exec("ffmpeg -y -ss ".$start." -i \"$filepath\" -to ".$duration." -vf scale=640:-1 -c:v libx264 ".$mute." -preset fast ".$thumbpath);
 }
 
 header('Content-type: video/mp4');
