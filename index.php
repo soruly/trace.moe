3838a9638a7e6fb85bbb248868c40946739e725d
Teo Gonzalez Calzada <teo.gonzalez.c@hotmail.com>
2018-12-19 23:36:46 -0700

    Change layout to make it responsive

--- index.php
+++ index.php
@@ -45,89 +45,99 @@ if(isset($_GET["url"]) && filter_var($_GET["url"], FILTER_VALIDATE_URL))
 <script src="/js/analytics.js" async defer></script>
 </head>
 <body>
-<div class="alert alert-warning" style="position: relative; top: 10px; left: 10px; box-shadow: 0 0 20px 0px; width: 975px">
-  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-New Domain: <a href="https://trace.moe">https://trace.moe</a>
+<div class="container">
+<div class="row">
+    <div class="col-md-10 col-centered alert alert-info" style="box-shadow: 0 0 20px 0px;    ">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+		  <a href="https://www.patreon.com/posts/moving-to-new-22212117">See why whatanime.ga moved to trace.moe</a>
+    </div>
 </div>
+
 <input id="autoSearch" type="checkbox" style="display: none;">
 <img id="originalImage" src="" crossorigin="anonymous" style="display: none;">
+
 <div class="row">
-<div class="col-xs-8">
-<div id="main">
-<div class="noselect">
-<div id="loading" class="hidden">
-<div id="loader" class="ripple"></div>
-</div>
-<canvas id="preview" width="640" height="360"></canvas>
-<video id="player" style="display:none" volume="0.5" autoplay></video>
-</div>
-<div style="height: 15px">
-  <span id="progressBarControl" class="glyphicon glyphicon-triangle-top"></span>
-  <span id="fileNameDisplay"></span>
-  <span id="timeCodeDisplay"></span>
-</div>
-<div id="form">
-<span class="btn btn-default btn-file btn-sm">
-Browse a file <input type="file" id="file" name="files[]" />
-</span>
-<span id="instruction"> / Drag &amp; Drop Anime ScreenShot / Ctrl+V / Enter Image URL</span>
-<button id="flipBtn" type="button" class="btn btn-default btn-sm" disabled>
-<span class="glyphicon glyphicon-unchecked"></span> Flip Image
-</button>
-<br>
-<form method="post">
-<input type="url" pattern="https?://.+" name="imageURL" class="form-control" id="imageURL" placeholder="Image URL" style="margin:5px 0 5px 0">
-<input type="submit" id="submit" style="display:none">
-</form>
-<div style="text-align: right">
-<span id="messageText" style="float:left;line-height:30px"></span>
-<label for="seasonSelector" style="font-weight: inherit">Search in (anilist ID):</label>
-<input type="text" id="seasonSelector" class="form-control input-sm" style="display:inline-block; width:100px">
+	<div id="main" class="col-md-8">
+		<div class="noselect">
+			<div id="loading" class="hidden">
+				<div id="loader" class="ripple"></div>
+			</div>
+			<canvas id="preview" width="640" height="360"></canvas>
+			<video id="player" style="display:none" volume="0.5" autoplay></video>
+		</div>
 
-<button id="safeBtn" type="button" class="btn btn-default btn-sm">
-<span class="glyphicon glyphicon-unchecked"></span> Safe Search
-</button>
-<button id="searchBtn" type="button" class="btn btn-default btn-sm btn-primary" disabled>
-<span class="glyphicon glyphicon-search"></span> Search
-</button>
-</div>
-Please read <a href="/faq">FAQ</a> to understand what can / cannot be searched.<br>
-<span style="color:#FF6D6D">Caution: some results may be NSFW (Not Safe for Work).</span><br>
-Official WebExtension available on <a href="https://chrome.google.com/webstore/detail/search-anime-by-screensho/gkamnldpllcbiidlfacaccdoadedncfp" target="_blank">Chrome</a>, <a href="https://addons.mozilla.org/en-US/firefox/addon/search-anime-by-screenshot/" target="_blank">Firefox</a>, and <a href="https://addons.opera.com/en/extensions/details/search-anime-by-screenshot/" target="_blank">Opera</a>.<br>
-Official Telegram Bot available <a href="https://telegram.me/WhatAnimeBot">@WhatAnimeBot</a><br>
-</div>
-</div>
-<div id="info"></div>
+		<div style="height: 15px">
+			<span id="progressBarControl" class="glyphicon glyphicon-triangle-top"></span>
+			<span id="fileNameDisplay"></span>
+			<span id="timeCodeDisplay"></span>
+		</div>
 
-<br>
+		<div id="form">
+			<span class="btn btn-default btn-file btn-sm">
+				Browse a file <input type="file" id="file" name="files[]" />
+			</span>
+			<span id="instruction"> / Drag &amp; Drop Anime ScreenShot / Ctrl+V / Enter Image URL</span>
+			<button id="flipBtn" type="button" class="btn btn-default btn-sm" disabled>
+			<span class="glyphicon glyphicon-unchecked"></span> Flip Image
+			</button>
+			<br>
+			<form method="post">
+				<input type="url" pattern="https?://.+" name="imageURL" class="form-control" id="imageURL" placeholder="Image URL" style="margin:5px 0 5px 0">
+				<input type="submit" id="submit" style="display:none">
+			</form>
+
+			<div style="text-align: right">
+				<span id="messageText" style="float:left;line-height:30px"></span>
+				<label for="seasonSelector" style="font-weight: inherit">Search in (anilist ID):</label>
+				<input type="text" id="seasonSelector" class="form-control input-sm" style="display:inline-block; width:100px">
+
+				<button id="safeBtn" type="button" class="btn btn-default btn-sm">
+				<span class="glyphicon glyphicon-unchecked"></span> Safe Search
+				</button>
+				<button id="searchBtn" type="button" class="btn btn-default btn-sm btn-primary" disabled>
+				<span class="glyphicon glyphicon-search"></span> Search
+				</button>
+			</div>
+
+			Please read <a href="/faq">FAQ</a> to understand what can / cannot be searched.<br>
+			<span style="color:#FF6D6D">Caution: some results may be NSFW (Not Safe for Work).</span><br>
+			Official WebExtension available on <a href="https://chrome.google.com/webstore/detail/search-anime-by-screensho/gkamnldpllcbiidlfacaccdoadedncfp" target="_blank">Chrome</a>, <a href="https://addons.mozilla.org/en-US/firefox/addon/search-anime-by-screenshot/" target="_blank">Firefox</a>, and <a href="https://addons.opera.com/en/extensions/details/search-anime-by-screenshot/" target="_blank">Opera</a>.<br>
+			Official Telegram Bot available <a href="https://telegram.me/WhatAnimeBot">@WhatAnimeBot</a><br>
+		</div>
+	</div><!-- /#main -->
+	<div class="col-md-3" id="results-container">
+		<div id="results-list">
+			<div id="controls" class="checkbox">
+				<label><input type="checkbox" id="autoplay" name="autoplay" checked />AutoPlay</label>
+				<label><input type="checkbox" id="loop" name="loop" />Loop</label>
+				<label><input type="checkbox" id="mute" name="mute" />Mute</label>
+			</div>
+			<ul id="results" class="nav nav-pills nav-stacked"></ul>
+		</div>
+	</div>
+</div><!-- /.row -->
+<div class="row">
+	<div id="info" class="col-md-8 pull-left">Some info of the project</div>
 </div>
-<div class="col-xs-4">
-  <div id="results-list">
-    <div id="controls" class="checkbox">
-      <label><input type="checkbox" id="autoplay" name="autoplay" checked />AutoPlay</label>
-      <label><input type="checkbox" id="loop" name="loop" />Loop</label>
-      <label><input type="checkbox" id="mute" name="mute" />Mute</label>
-    </div>
-    <ul id="results" class="nav nav-pills nav-stacked"></ul>
-  </div>
-</div>
-</div>
+</div><!-- /.container-->
+<!-- Github link -->
 <a href="https://github.com/soruly/trace.moe" class="github-corner" aria-label="View source on Github">
 <svg width="80" height="80" viewBox="0 0 250 250" style="fill:#151513; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg>
 </a>
 <style>.github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}</style>
-
+<!-- Github link -->
 <footer class="footer">
-<div class="container">
-<ol class="breadcrumb">
-<li><a href="/">Home</a></li>
-<li><a href="/about">About</a></li>
-<li><a href="/changelog">Changelog</a></li>
-<li><a href="/faq">FAQ</a></li>
-<li><a href="/terms" class="active">Terms</a></li>
-</ol>
-</div>
+	<div class="container">
+		<ol class="breadcrumb">
+			<li><a href="/">Home</a></li>
+			<li><a href="/about">About</a></li>
+			<li><a href="/changelog">Changelog</a></li>
+			<li><a href="/faq">FAQ</a></li>
+			<li><a href="/terms" class="active">Terms</a></li>
+		</ol>
+	</div>
 </footer>
+
 <script src="/js/jquery-3.2.1.min.js"></script>
 <script src="/js/bootstrap.min.js"></script>
 <script src="/js/index.js"></script>
