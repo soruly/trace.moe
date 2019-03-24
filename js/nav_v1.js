848f25dd727ca7e5ea87db307db6f3885a99d804
Emmanuel See Te <bsit.emmanuel@gmail.com>
2019-03-24 15:02:31 +0800

    removed nav_v1.js, using css instead

--- js/nav_v1.js
+++
@@ -1,8 +0,0 @@
-function waitNav() {
-    var nav = document.getElementById("wait-nav");
-    if (nav.style.display === "flex") {
-        nav.style.display = "none";
-    } else {
-        nav.style.display = "flex";
-    }
-}
