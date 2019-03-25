ffac27297f42082e24fcfdf0a762b04cfe742a95
Emmanuel See Te <bsit.emmanuel@gmail.com>
2019-03-25 17:51:54 +0800

    Style update for search, results list, anime info

--- index.php
+++ index.php
@@ -75,7 +75,7 @@ if (isset($_GET["url"]) && filter_var($_GET["url"], FILTER_VALIDATE_URL)) {
                 <span class="glyphicon glyphicon-menu-hamburger"></span>
             </a>
             <div class="navbar__menu">
-                <a class="navbar__item navbar__item--active" href="/" >Home</a>
+                <a class="navbar__item navbar__item--active" href="/">Home</a>
                 <a class="navbar__item" href="/about">About</a>
                 <a class="navbar__item" href="/changelog">Changelog</a>
                 <a class="navbar__item" href="/faq">FAQ</a>
@@ -89,12 +89,12 @@ if (isset($_GET["url"]) && filter_var($_GET["url"], FILTER_VALIDATE_URL)) {
             <p class="w-alert__text">Read recent updates to trace.moe | soruly on <a href="https://www.patreon.com/posts/24430008" class="w-alert__link">Patreon!</a></p>
         </div><!-- /.width -->
     </div><!-- /.w-alert -->
-    
+
     <main>
         <input id="autoSearch" type="checkbox" style="display: none;" <?php echo $autosearch ? "checked" : ""; ?>>
         <img id="originalImage" src="<?php echo $originalImage; ?>" crossorigin="anonymous" style="display: none;">
 
-        <div id="main">
+        <div id="main" class="card card-search">
             <div class="noselect">
                 <div id="loading" class="hidden">
                     <div id="loader" class="ripple"></div>
@@ -102,63 +102,66 @@ if (isset($_GET["url"]) && filter_var($_GET["url"], FILTER_VALIDATE_URL)) {
                 <canvas id="preview" width="640" height="360"></canvas>
                 <video id="player" style="display:none" volume="0.5" autoplay></video>
             </div>
-            <div class="main-filesource">
+
+            <div class="search__source">
                 <span id="progressBarControl" class="glyphicon glyphicon-triangle-top"></span>
                 <span id="fileNameDisplay"></span>
                 <span id="timeCodeDisplay"></span>
-            </div>
-            <div id="form">
-                <span class="btn btn-default btn-file btn-sm">
-                    <span class="glyphicon glyphicon-folder-open"></span>&nbsp;&nbsp;Browse a file <input type="file" id="file" name="files[]" />
-                </span>
-                <button id="flipBtn" type="button" class="btn btn-default btn-sm" disabled>
-                    <span class="glyphicon glyphicon-unchecked"></span> Flip Image
-                </button>
-                <span id="instruction"> / Drag &amp; Drop Anime ScreenShot / Ctrl+V / Enter Image URL</span>
-                <br>
-                <form method="post">
-                    <input type="url" pattern="https?://.+" name="imageURL" class="form-control input-sm" id="imageURL" placeholder="Image URL" value="<?php echo $imageURL; ?>" style="margin:5px 0 5px 0">
-                    <input type="submit" id="submit" style="display:none">
-                </form>
-                <div style="text-align: right">
-                    <span id="messageText" style="float:left;line-height:30px"></span>
-                    <label for="seasonSelector" style="font-weight: inherit">Search in (anilist ID):</label>
-                    <input type="text" id="seasonSelector" class="form-control input-sm" style="display:inline-block; width:100px" placeholder="anilist ID">
+            </div> <!-- /.search__source -->
 
+            <div id="form" class="search__form">
+                <span id="instruction" class="search__instruction">Drag &amp; Drop Anime ScreenShot / Ctrl+V / Enter Image URL</span>
+
+                <form method="post" class="search__input-group">
+                    <!-- <label for="seasonSelector" style="font-weight: inherit">Search in (anilist ID):</label> -->
+                    <input type="text" id="seasonSelector" class="form-control input-sm search__input" placeholder="anilist ID (Optional)">
+                    <input type="url" pattern="https?://.+" name="imageURL" class="form-control input-sm search__input" id="imageURL" placeholder="Image URL" value="<?php echo $imageURL; ?>">
+                    <input type="submit" id="submit" style="display:none">
+                </form><!-- /.search__input-group -->
+
+                <div class="search__button-group">
+                    <span id="messageText" style="float:left;line-height:30px"></span>
+                    <span class="btn btn-default btn-file btn-sm">
+                        Browse a file <input type="file" id="file" name="files[]" />
+                    </span>
                     <button id="safeBtn" type="button" class="btn btn-default btn-sm">
                         <span class="glyphicon glyphicon-unchecked"></span> Safe Search
+                    </button>
+                    <button id="flipBtn" type="button" class="btn btn-default btn-sm" disabled>
+                        <span class="glyphicon glyphicon-unchecked"></span> Flip Image
                     </button>
                     <button id="searchBtn" type="button" class="btn btn-default btn-sm btn-primary" disabled>
                         <span class="glyphicon glyphicon-search"></span> Search
                     </button>
-                </div>
-            </div>
-            <div class="main-footer">
-                <span class="main-footer__warning">Caution: Some results may be NSFW (Not Safe for Work)</span><br>
+                </div><!-- /.search__button-group -->
+            </div><!-- /.search__form -->
+
+            <div class="search__footer">
+                <span class="main-footer__warning">Warning: Some results may be NSFW (Not Safe for Work)</span><br>
                 <span><a href="https://telegram.me/WhatAnimeBot">Official Telegram Bot</a> | Official WebExtension for <a href="https://chrome.google.com/webstore/detail/search-anime-by-screensho/gkamnldpllcbiidlfacaccdoadedncfp">Chrome</a>, <a href="https://addons.mozilla.org/en-US/firefox/addon/search-anime-by-screenshot/">Firefox</a>, and <a href="https://addons.opera.com/en/extensions/details/search-anime-by-screenshot/">Opera</a><br></span>
-            </div>
-        </div>
+            </div><!-- /.search__footer -->
+        </div><!-- /.card .card-search -->
 
-
-        <div id="results-list">
+        <div id="results-list" class="card">
             <div id="controls" class="checkbox">
                 <label><input type="checkbox" id="autoplay" name="autoplay" checked />AutoPlay</label>
                 <label><input type="checkbox" id="loop" name="loop" />Loop</label>
                 <label><input type="checkbox" id="mute" name="mute" />Mute</label>
             </div>
             <ul id="results" class="nav nav-pills nav-stacked"></ul>
-        </div>
+        </div><!--  /.card -->
 
-        <div id="info"></div>
-
-        <a href="https://github.com/soruly/trace.moe" class="github-corner" aria-label="View source on Github">
-            <svg width="58" height="58" viewBox="0 0 250 250" style="fill:#151513; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true">
-                <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
-                <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path>
-                <path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path>
-            </svg>
-        </a>
+        <div id="info" class="card card-info"></div><!-- /.card .card-info -->
     </main>
+
+    <a href="https://github.com/soruly/trace.moe" class="github-corner" aria-label="View source on Github">
+        <svg width="67" height="67" viewBox="0 0 250 250" style="fill:#151513; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true">
+            <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
+            <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path>
+            <path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path>
+        </svg>
+    </a>
+
     <script src="/js/index_v3.js"></script>
     <script src="/js/info_v3.js"></script>
 </body>
